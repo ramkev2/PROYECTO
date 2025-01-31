7d418c2e43f461c59dfff4368af7070a56085cd8
Ashley Martin <ashleyrilo@hotmail.com>
2025-01-31 10:46:37 +0100

    l

--- Proyecto/src/Controller/Controladores.php
+++ Proyecto/src/Controller/Controladores.php
@@ -15,14 +15,42 @@ use Symfony\Component\Mime\Email;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Usuario;
 
 class Controladores extends AbstractController
 {
 	#[Route('/login', name:'login')]
     public function login(){    
         return $this->render('login.html.twig');
-    }    
+    } 
+    #[Route('/ctr_login', name:'ctr_login')]
+    public function ctr_login(  Request $request, 
+    EntityManagerInterface $entityManager, 
+    UserPasswordHasherInterface $passwordHasher,)
+    {
+        $correo = $request->request->get('username');
+        $clave = $request->request->get('password');
+    
+        // Buscar usuario por email
+        $usuario = $entityManager->getRepository(Usuario::class)->findOneBy(['email' => $correo]);
+    
+        if (!$usuario) {
+            return $this->render('login.html.twig', ['error' => 'Usuario no encontrado']);
+        }
+    
+        // Verificar contraseña
+        if (!$passwordHasher->isPasswordValid($usuario, $clave)) {
+            return $this->render('login.html.twig', ['error' => 'Contraseña incorrecta']);
+        }
+    
+       return $this->render('inicio.html.twig');
+    
+    
+    }       
 	
 	#[Route('/logout', name:'logout')]
     public function logout(){    
@@ -30,14 +58,11 @@ class Controladores extends AbstractController
     }   
     
 
-    #[Route('/registrarse', name:'registrarse')]
-    public function registrarse(){    
-        return new Response();
-    }
-
+   
     #[Route('/recuperarContraseña', name:'recuperarContraseña')]
     public function recuperarContraseña(){    
         return new Response();
     }
     
 }
+
